2cfcbef676144088c07e351c6f47018251333585
z23091820-ux <z23091820@gmail.com>
2026-07-27 23:18:28 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// 請將 Firebase Console 提供的 firebaseConfig 貼到這裡。
-// 設定完成前，網站仍可使用原本的單機模式，但無法登入與家庭同步。
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAD85bhFRThQbxFKqIw733TMBstzCNPLig",
+  authDomain: "z23091820-41fd5.firebaseapp.com",
+  projectId: "z23091820-41fd5",
+  storageBucket: "z23091820-41fd5.firebasestorage.app",
+  messagingSenderId: "276579343112",
+  appId: "1:276579343112:web:3d32c90f95d7d9818faabd",
+  measurementId: "G-YHG9DHFRZ0"
 };
